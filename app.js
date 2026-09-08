f732c6bac6488ac3fc6002dc678416376e61a28c
Hamid00Fathi <166663270+Hamid00Fathi@users.noreply.github.com>
2026-09-08 16:14:33 +0330

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ async function loadStatus() {
         const res = await fetch(`https://gamenet-server-mongo.onrender.com/status/${username}`);
         const data = await res.json();
 
-        /* نمایش مستقیم زمان بدون تبدیل */
+        // نمایش مستقیم زمان بدون تبدیل
         document.getElementById("lastUpdate").innerText =
             "آخرین آپدیت: " + data.lastUpdate;
 
@@ -88,8 +88,11 @@ function renderCustomer(c) {
 }
 
 function openModal(html) {
-    document.getElementById("modalContent").innerHTML = html;
-    document.getElementById("modal").style.display = "block";
+    const modal = document.getElementById("modal");
+    const content = document.getElementById("modalContent");
+
+    content.innerHTML = html;
+    modal.style.display = "block";
 }
 
 document.getElementById("closeModal").onclick = () => {
